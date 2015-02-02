3e533b9343e925a1d8dad81a926448ae69c166f2
cap <michal.cap@agents.fel.cvut.cz>
2015-02-02 09:58:18 +0800

    improved run-experiment.sh script

--- run-experiment.sh
+++ run-experiment.sh
@@ -1,31 +1,40 @@
 #!/bin/bash
 
-# compile the both the experimental framework
-echo -e "\nCompiling the codebase\n"
+# clean temporary files and previous results
+echo -e "\n >>>> Cleaning\n"
+mvn clean
+rm -f experiment/instances/empty-hall-r25/*.out.*
+rm -f experiment/instances/ubremen-r27/*.out.*
+rm -f experiment/instances/warehouse-r25/*.out.*
+rm -rf experiment/plots
+rm -f expermient/solver.jar
+
+# compile the experimental framework
+echo -e "\n >>>> Compiling the codebase\n"
 mvn package
 
-# copy the resulting binaries to the experiment folder
+# copy the binaries to the experiment folder
 cp target/map4rt-1.0-SNAPSHOT-jar-with-dependencies.jar experiment/solver.jar
 
 cd experiment
 
-CPUS=1 # no of CPU to be used for the experiment
+CPUS=1 # no of CPU to be used for the experiment (how many experiments to run in parallel)
 MEM=1  # maximum memory used by one simulation run in GBs
 
 
 # run the experiment in empty-hall environment
-echo -e "\nRunning the experiment in empty hall...\n"
+echo -e "\n >>>> Running the experiment in empty hall...\n"
 ./run.sh empty-hall-r25 $CPUS data.in data.out $MEM  
 ./addhead.sh empty-hall-r25
 
 
 # run the experiment in ubremen environment
-echo -e "\nRunning the experiment in ubremen...\n"
+echo -e "\n >>>> Running the experiment in ubremen...\n"
 ./run.sh ubremen-r27 $CPUS data.in data.out $MEM
 ./addhead.sh ubremen-r27
 
 # run the experiment in warehouse environment
-echo -e "\nRunning the experiment in warehouse...\n"  
+echo -e "\n >>>> Running the experiment in warehouse...\n"  
 ./run.sh warehouse-r25 $CPUS data.in data.out $MEM
 ./addhead.sh warehouse-r25
 
@@ -34,13 +43,4 @@ echo -e "\nRunning the experiment in warehouse...\n"
 mkdir -p plots
 Rscript makeplots.r
 
-echo -e "Done! The new plots have been generated in /plots directory."
-
-
-
-
-
-
-
-
-
+echo -e "\n >>>> Done! The new plots have been generated in /plots directory. \n"
